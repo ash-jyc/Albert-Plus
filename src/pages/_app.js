6b161333ae34d430e17d0f07958605c7368cc81d
Ashley <ajychen26@gmail.com>
2023-11-15 19:59:21 +0800

    deleted:    .gitignore 	deleted:    README.md 	deleted:    jsconfig.json 	deleted:    next.config.js 	deleted:    package-lock.json 	deleted:    package.json 	deleted:    src/components/academic-pathway.js 	deleted:    src/components/chat.js 	deleted:    src/components/footer.js 	deleted:    src/components/header.js 	deleted:    src/components/search-results.js 	deleted:    src/pages/_app.js 	deleted:    src/pages/index.js 	deleted:    src/styles/academic-pathway.module.css 	deleted:    src/styles/chat.module.css 	deleted:    src/styles/footer.module.css 	deleted:    src/styles/global.css 	deleted:    src/styles/header.module.css 	deleted:    src/styles/search-results.module.css
    	application/

--- src/pages/_app.js
+++
@@ -1,6 +0,0 @@
-// pages/_app.js
-import '@/styles/global.css'; // Add this line to include global CSS
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
